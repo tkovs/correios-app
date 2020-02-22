9892c1e785d3ef62d609b41775d541d9452594c8
tkovs <joao.rodrigues@arapiraca.ufal.br>
2020-02-22 02:42:37 -0300

    Move code to self component

--- App.js
+++ App.js
@@ -1,28 +1,6 @@
 import React from 'react'
-import { SafeAreaView, View, StyleSheet } from 'react-native'
-import { Appbar } from 'react-native-paper'
-import { colors } from './src/styles/theme'
+import Home from './src/pages/Home'
 
-const App = () => (
-  <SafeAreaView style={styles.container}>
-    <Appbar.Header style={styles.header}>
-      <Appbar.Content title="Correios" subtitle="Rastreio de pacotes" />
-    </Appbar.Header>
-    <View style={styles.body} />
-  </SafeAreaView>
-)
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  header: {
-    backgroundColor: colors.blue,
-  },
-  body: {
-    backgroundColor: colors.isabelline,
-    flex: 1,
-  },
-})
+const App = () => <Home />
 
 export default App
---
+++ src/pages/Home/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { SafeAreaView, View, StyleSheet } from 'react-native'
+import { Appbar } from 'react-native-paper'
+import { colors } from '../../styles/theme'
+
+const Home = () => (
+  <SafeAreaView style={styles.container}>
+    <Appbar.Header style={styles.header}>
+      <Appbar.Content title="Correios" subtitle="Rastreio de pacotes" />
+    </Appbar.Header>
+    <View style={styles.body} />
+  </SafeAreaView>
+)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    backgroundColor: colors.blue,
+  },
+  body: {
+    backgroundColor: colors.isabelline,
+    flex: 1,
+  },
+})
+
+export default Home
