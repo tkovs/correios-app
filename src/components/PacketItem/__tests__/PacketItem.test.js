a20825cbd3305886e4b5e36acfbb61cff8384486
tkovs <joao.rodrigues@arapiraca.ufal.br>
2020-02-25 00:04:40 -0300

    Test PacketItem component

---
+++ src/components/PacketItem/__tests__/PacketItem.test.js
@@ -0,0 +1,19 @@
+import 'react-native'
+import React from 'react'
+import { render } from '@testing-library/react-native'
+
+import PacketItem from '../PacketItem'
+
+it('renders correctly', () => {
+  const mockPacket = {
+    title: 'Apple Pencil',
+    status: 'delivered',
+    code: 'PW086958115BR',
+    mode: 'SEDEX',
+    createdAt: '07/02/2020',
+    updatedAt: '10/02/2020',
+    deliveredAt: '10/02/2020',
+  }
+  const { baseElement } = render(<PacketItem packet={mockPacket} />)
+  expect(baseElement).toMatchSnapshot()
+})
