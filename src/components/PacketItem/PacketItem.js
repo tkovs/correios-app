dfc455a533343840a44185e82affd8320addd004
tkovs <joao.rodrigues@arapiraca.ufal.br>
2020-02-25 00:09:43 -0300

    Improve PacketItem test covarage

--- src/components/PacketItem/PacketItem.js
+++ src/components/PacketItem/PacketItem.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Text, View, StyleSheet } from 'react-native'
 import PropTypes from 'prop-types'
+import isNil from 'lodash/isNil'
 
 import { colors } from '../../styles/theme'
 
 const PacketItem = ({ packet }) => {
+  if (isNil(packet)) {
+    return null
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{packet.title}</Text>
--- src/components/PacketItem/__tests__/PacketItem.test.js
+++ src/components/PacketItem/__tests__/PacketItem.test.js
@@ -4,16 +4,28 @@ import { render } from '@testing-library/react-native'
 
 import PacketItem from '../PacketItem'
 
-it('renders correctly', () => {
-  const mockPacket = {
-    title: 'Apple Pencil',
-    status: 'delivered',
-    code: 'PW086958115BR',
-    mode: 'SEDEX',
-    createdAt: '07/02/2020',
-    updatedAt: '10/02/2020',
-    deliveredAt: '10/02/2020',
-  }
-  const { baseElement } = render(<PacketItem packet={mockPacket} />)
-  expect(baseElement).toMatchSnapshot()
+describe('PacketItem', () => {
+  describe('without a packet', () => {
+    it('renders correctly', () => {
+      const mockPacket = null
+      const { baseElement } = render(<PacketItem packet={mockPacket} />)
+      expect(baseElement).toMatchSnapshot()
+    })
+  })
+
+  describe('with full packet', () => {
+    it('renders correctly', () => {
+      const mockPacket = {
+        title: 'Apple Pencil',
+        status: 'delivered',
+        code: 'PW086958115BR',
+        mode: 'SEDEX',
+        createdAt: '07/02/2020',
+        updatedAt: '10/02/2020',
+        deliveredAt: '10/02/2020',
+      }
+      const { baseElement } = render(<PacketItem packet={mockPacket} />)
+      expect(baseElement).toMatchSnapshot()
+    })
+  })
 })
