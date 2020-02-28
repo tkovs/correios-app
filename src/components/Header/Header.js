aaf92a8b2825bbd1c53bb483beadb361927da300
tkovs <joao.rodrigues@arapiraca.ufal.br>
2020-02-28 01:22:08 -0300

    Change statusBar background color

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, StatusBar } from 'react-native'
 import { Appbar } from 'react-native-paper'
 import { colors } from '../../styles/theme'
 
 const Header = () => (
   <Appbar.Header style={styles.header}>
+    <StatusBar backgroundColor={colors.blue} />
     <Appbar.Content title="Correios" subtitle="Rastreio de pacotes" />
   </Appbar.Header>
 )
