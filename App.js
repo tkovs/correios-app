712a36a1f1b689b3624aad3f4af2aa84740f18b7
tkovs <joao.rodrigues@arapiraca.ufal.br>
2020-03-02 01:07:41 -0300

    Update CHANGELOG

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { View, Text } from 'react-native'
 
 import { createStackNavigator } from '@react-navigation/stack'
-import Home from './src/pages/Home'
+import Home from './src/screens/Home'
 
 const Stack = createStackNavigator()
 
