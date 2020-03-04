db9283d1fea4f547dd8acd7e43ac7e79f566ea16
tkovs <joao.rodrigues@arapiraca.ufal.br>
2020-03-03 23:56:19 -0300

    Improve tests for Header

--- src/components/Header/__tests__/Header.test.js
+++ src/components/Header/__tests__/Header.test.js
@@ -4,9 +4,20 @@ import { render } from '@testing-library/react-native'
 
 import Header from '../Header'
 
-it('renders correctly', () => {
-  const { baseElement } = render(
-    <Header title="Correios" subtitle="Rastreio de pacotes" />
-  )
-  expect(baseElement).toMatchSnapshot()
+describe('Header', () => {
+  describe('with title and subtitle', () => {
+    it('renders correctly', () => {
+      const { baseElement } = render(
+        <Header title="Correios" subtitle="Rastreio de pacotes" />
+      )
+      expect(baseElement).toMatchSnapshot()
+    })
+  })
+
+  describe('just with title', () => {
+    it('renders correctly', () => {
+      const { baseElement } = render(<Header title="Correios" />)
+      expect(baseElement).toMatchSnapshot()
+    })
+  })
 })
