91aa5944dd177b040312a1394fa1e0f1be0a07a9
tkovs <joao.rodrigues@arapiraca.ufal.br>
2020-03-19 02:17:11 -0300

    Add some style to packet info screen

--- src/components/PacketTrackingItem/PacketTrackingItem.js
+++ src/components/PacketTrackingItem/PacketTrackingItem.js
@@ -1,20 +1,46 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { View } from 'react-native'
+import moment from 'moment'
+import { View, StyleSheet } from 'react-native'
 import { Caption, Text, Title } from 'react-native-paper'
 
 function PacketTrackingItem({ date, status, from, to, location, note }) {
+  const time = moment(date).format('h:mm')
+  const day = moment(date).format('D MMM')
+
   return (
-    <View>
-      <Caption>{date.toString()}</Caption>
-      <Title>{status}</Title>
-      <Text>{from}</Text>
-      <Text>{to}</Text>
-      <Text>{location}</Text>
-      <Caption>Obs.: {note}</Caption>
+    <View style={styles.container}>
+      <View style={styles.left}>
+        <Text>{time}</Text>
+        <Text>{day}</Text>
+      </View>
+      <View style={styles.right}>
+        <Title>{status}</Title>
+        <Text>De: {from}</Text>
+        {to ? <Text>Para: {to}</Text> : null}
+        <Text>{location}</Text>
+        {note ? <Caption>Obs.: {note}</Caption> : null}
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderBottomColor: 'gray',
+  },
+  left: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRightWidth: 1,
+    borderRightColor: 'gray',
+    paddingRight: 8,
+    marginRight: 8,
+  },
+})
 
 PacketTrackingItem.propTypes = {
   status: PropTypes.string.isRequired,
