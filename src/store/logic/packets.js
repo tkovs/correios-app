f2b12b4d08841107e658548f5736a479d4913988
tkovs <joao.rodrigues@arapiraca.ufal.br>
2020-03-31 00:47:03 -0300

    Fetch tracking info from api

--- src/store/logic/packets.js
+++ src/store/logic/packets.js
@@ -1,19 +1,15 @@
 import { createLogic } from 'redux-logic'
+import correios from 'encomendas-correios/lib/index'
 
 import * as types from '../actions/packets/types'
 import { addPacket } from '../actions/packets'
-import mockPackets from '../../__mocks__/packets'
-
-const track = () => {
-  return new Promise(resolve => resolve(mockPackets[0].statuses))
-}
 
 const fetchPacketLogic = createLogic({
   type: types.FETCH_PACKET,
   process: async ({ action }, dispatch, done) => {
     const { code, title } = action.payload
 
-    const statuses = await track(code)
+    const statuses = await correios.track(code)
     const packet = {
       title,
       code,
