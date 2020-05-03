4faa8d69bbc02b09a25dd3b1e80a48c16b20c826
tkovs <joao.rodrigues@arapiraca.ufal.br>
2020-05-02 22:39:35 -0300

    Improve the InfoBar component spacing

--- src/screens/PacketInfo/InfoBar/InfoBar.js
+++ src/screens/PacketInfo/InfoBar/InfoBar.js
@@ -10,19 +10,21 @@ import { colors } from '../../../styles/theme'
 function InfoBar({ code, passedDays, shippingMode }) {
   return (
     <View style={styles.container}>
-      <View style={styles.info}>
+      <View style={styles.smallInfo}>
         <FontAwesomeIcon name="road" size={20} color={colors.blue} />
         <Text style={styles.text}>
           {passedDays} {passedDays > 1 ? 'dias' : 'dia'}
         </Text>
       </View>
-      <View style={styles.info}>
+      <View style={styles.bigInfo}>
         <FontAwesomeIcon name="barcode" size={20} color={colors.blue} />
         <Text style={styles.text}>{code}</Text>
       </View>
-      <View style={styles.info}>
+      <View style={styles.smallInfo}>
         <MaterialIcon name="local-shipping" size={20} color={colors.blue} />
-        <Text style={styles.text}>{shippingMode}</Text>
+        <Text style={styles.text} numberOfLines={1}>
+          {shippingMode}
+        </Text>
       </View>
     </View>
   )
@@ -36,15 +38,19 @@ InfoBar.propTypes = {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: colors.sandstorm,
     alignItems: 'center',
+    backgroundColor: colors.sandstorm,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    paddingHorizontal: 24,
     paddingVertical: 8,
   },
-  info: {
+  smallInfo: {
     alignItems: 'center',
+    width: '30%',
+  },
+  bigInfo: {
+    alignItems: 'center',
+    width: '40%',
   },
   text: {
     color: colors.blue,
