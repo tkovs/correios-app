913b4dc60fb43b963e52f0b351d2136872ffac07
tkovs <joao.rodrigues@arapiraca.ufal.br>
2020-05-03 00:15:55 -0300

    Add icon to blank screen packet list

--- src/components/PacketList/PacketList.js
+++ src/components/PacketList/PacketList.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { View, StyleSheet, ScrollView } from 'react-native'
-import { Divider } from 'react-native-paper'
+import { View, StyleSheet, ScrollView, Dimensions } from 'react-native'
+import { Divider, Caption } from 'react-native-paper'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 import isEmpty from 'lodash/isEmpty'
 
 import PacketItem from '../PacketItem'
 
 const PacketList = ({ packets }) => {
+  const screenWidth = Dimensions.get('window').width
+  const iconSize = screenWidth * 0.8
+  const iconColor = '#ddd'
+
   if (isEmpty(packets)) {
-    return null
+    return (
+      <View style={styles.iconScreen}>
+        <Icon name="error-outline" size={iconSize} color={iconColor} />
+        <Caption>Nenhuma encomenda nesta lista</Caption>
+      </View>
+    )
   }
 
   return (
@@ -26,6 +36,12 @@ const PacketList = ({ packets }) => {
 const styles = StyleSheet.create({
   container: {
     paddingHorizontal: 8,
+  },
+  iconScreen: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
   },
 })
 
