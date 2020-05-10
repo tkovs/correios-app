d34cf34d0013b491e49faf5f2e787b3f140f9fbc
tkovs <joao.rodrigues@arapiraca.ufal.br>
2020-05-10 20:37:09 -0300

    Add the first test for feedback logic

---
+++ src/store/logic/__tests__/feedback.test.js
@@ -0,0 +1,37 @@
+import { createMockStore } from 'redux-logic-test'
+
+import rootReducer from '../../reducers'
+import feedback from '../feedback'
+import { addFeedback, clearFeedback } from '../../actions/feedback'
+
+let store = null
+
+describe('Feedback Logic', () => {
+  beforeEach(() => {
+    store = createMockStore({ logic: [...feedback], reducer: rootReducer })
+    store.resetActions()
+  })
+
+  describe('clearFeedbackLogic', () => {
+    it('should allow clearFeedback if feedback is visible', done => {
+      const mockMessage = 'mockMessage'
+
+      store.dispatch(addFeedback(mockMessage))
+      store.whenComplete(() => {
+        expect(store.getState().feedback.message).toEqual(mockMessage)
+        expect(store.getState().feedback.visible).toBeTruthy()
+
+        store.dispatch(clearFeedback())
+        store.whenComplete(() => {
+          expect(store.getState().feedback.message).toEqual('')
+          expect(store.getState().feedback.visible).toBeFalsy()
+          expect(store.actions).toHaveLength(2)
+          expect(store.actions[0]).toEqual(addFeedback(mockMessage))
+          expect(store.actions[1]).toEqual(clearFeedback())
+
+          done()
+        })
+      })
+    })
+  })
+})
