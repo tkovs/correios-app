b05b876845d95833ad231cfb75fc1b23765d2130
tkovs <joao.rodrigues@arapiraca.ufal.br>
2020-05-11 13:39:39 -0300

    Add test for archivePacketLogic

--- src/store/logic/__tests__/packets.test.js
+++ src/store/logic/__tests__/packets.test.js
@@ -2,18 +2,11 @@ import { createMockStore } from 'redux-logic-test'
 
 import rootReducer from '../../reducers'
 import packetsLogic from '../packets'
-import { addPacketSuccess } from '../../actions/packets'
+import { addPacketSuccess, archivePacket } from '../../actions/packets'
 import { addFeedback } from '../../actions/feedback'
+import mockPackets from '../../../__mocks__/packets'
 
-const mockPacket = {
-  title: 'iPhone 7',
-  code: 'PW086958109BR',
-  mode: 'SEDEX',
-  statuses: [],
-  createdAt: new Date(2020, 2, 2, 9, 5),
-  lastView: new Date(2020, 2, 4, 18, 13),
-  updatedAt: new Date(2020, 2, 3, 18, 13),
-}
+const mockPacket = mockPackets[0]
 
 describe('Packets Logic', () => {
   let store = null
@@ -36,6 +29,31 @@ describe('Packets Logic', () => {
         expect(store.actions[0]).toEqual(addPacketSuccess(mockPacket))
         expect(store.actions[1]).toEqual(addFeedback(expectedMessage))
         done()
+      })
+    })
+  })
+
+  describe('archivePacketLogic', () => {
+    it('should dispatch addFeedback action if packet was archived', done => {
+      const expectedMessage1 = 'Encomenda rastreada com sucesso'
+      const expectedMessage2 = 'Encomenda arquivada com sucesso'
+
+      store.dispatch(addPacketSuccess(mockPacket))
+      store.whenComplete(() => {
+        expect(store.getState().packets.list[0].code).toEqual(mockPacket.code)
+        expect(store.getState().packets.list[0].archived).toBeFalsy()
+
+        store.dispatch(archivePacket(mockPacket.code))
+        store.whenComplete(() => {
+          expect(store.getState().packets.list[0].code).toEqual(mockPacket.code)
+          expect(store.getState().packets.list[0].archived).toBeTruthy()
+          expect(store.actions).toHaveLength(4)
+          expect(store.actions[0]).toEqual(addPacketSuccess(mockPacket))
+          expect(store.actions[1]).toEqual(addFeedback(expectedMessage1))
+          expect(store.actions[2]).toEqual(archivePacket(mockPacket.code))
+          expect(store.actions[3]).toEqual(addFeedback(expectedMessage2))
+          done()
+        })
       })
     })
   })
