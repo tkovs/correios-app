30b28cf562eb4f491d4e632581b2cedb5a1e9943
tkovs <joao.rodrigues@arapiraca.ufal.br>
2020-05-17 15:58:25 -0300

    Migrate PacketItemContainer to typescript

---
+++ src/components/PacketItem/index.tsx
@@ -0,0 +1,44 @@
+import React, { FC } from 'react'
+import { connect, MapStateToProps } from 'react-redux'
+import { useNavigation } from '@react-navigation/native'
+
+import PacketItem from './PacketItem'
+import { statusListSelector } from '../../store/selectors/packets'
+import { Packet } from '../../types/Packet'
+import { StatusListItem } from '../../types/StatusListItem'
+
+interface StateProps {
+  statusList: StatusListItem[]
+}
+
+interface OwnProps {
+  packet: Packet
+}
+
+type Props = StateProps & OwnProps
+
+const PacketItemContainer: FC<Props> = (props: Props) => {
+  const { packet, statusList } = props
+  const navigation = useNavigation()
+
+  const navigateToPacketItemInfo: () => void = () =>
+    navigation.navigate('PacketInfo', { code: packet.code })
+
+  return (
+    <PacketItem
+      onClick={navigateToPacketItemInfo}
+      packet={packet}
+      statusList={statusList}
+    />
+  )
+}
+
+const mapStateToProps: MapStateToProps<StateProps, OwnProps> = (state) => ({
+  statusList: statusListSelector(state),
+})
+
+PacketItemContainer.defaultProps = {
+  statusList: [],
+}
+
+export default connect(mapStateToProps)(PacketItemContainer)
