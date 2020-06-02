b34e3a2287b1ced69e33a05ef1916cc0d8e09a78
tkovs <joao.rodrigues@arapiraca.ufal.br>
2020-06-02 00:20:14 -0300

    Use StandardAction in feedback action types

--- src/store/actions/feedback/types.ts
+++ src/store/actions/feedback/types.ts
@@ -1,17 +1,21 @@
-import { Action } from 'redux'
+import { StandardAction } from 'redux-logic'
 
 export const ADD_FEEDBACK = 'ADD_FEEDBACK'
 export const CLEAR_FEEDBACK = 'CLEAR_FEEDBACK'
 export const REJECT_CLEAR_FEEDBACK = 'REJECT_CLEAR_FEEDBACK'
 
-export interface AddFeedback extends Action<typeof ADD_FEEDBACK> {
+export interface AddFeedback extends StandardAction<typeof ADD_FEEDBACK> {
   payload: {
     message: string
   }
 }
-export type ClearFeedback = Action<typeof CLEAR_FEEDBACK>
-export type RejectClearFeedback = Action<typeof REJECT_CLEAR_FEEDBACK>
-export type FeedbackActionTypes = AddFeedback | ClearFeedback
+export type ClearFeedback = StandardAction<typeof CLEAR_FEEDBACK>
+export type RejectClearFeedback = StandardAction<typeof REJECT_CLEAR_FEEDBACK>
+
+export type FeedbackActionTypes =
+  | AddFeedback
+  | ClearFeedback
+  | RejectClearFeedback
 
 export interface FeedbackState {
   message: string
